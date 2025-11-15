256a55d2e9fde589831bf3a79e20599fe6a284a6
Kiel Astillasa <joannamarie.mabansag01@gmail.com>
2025-11-15 15:33:29 +0800

    added new printf statement

--- main.c
+++ main.c
@@ -3,6 +3,19 @@
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    int age = 20; //format specifier: %d
+    int chsd_asdh = 20;
+    int x 22;
+    char initial name = 'h'; //format specifier: %c
+    float ave_grade = 96.5; //format specifier: %f
+    float y = 49.54656;
+
+
+    printf("Age: %d \n", age);
+    printf("Initial letter of the name: %c \n", initial_name);
+    printf{"Average Grade: %f \n", ave_grade};
+    printf{"Float with Specific no. of deciman %.2f", ave_grade};
+    printf("sample changes");
+
+     return 0;
 }
